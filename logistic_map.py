302954693a21ae547ed02325308c4784b6819699
ASPP Student <student@localhost>
2021-08-24 12:48:06 +0200

    task b) implement function iterate_f and testing

--- logistic_map.py
+++ logistic_map.py
@@ -5,3 +5,21 @@ def logistic_map(x, r):
     y = r * x * (1 - x)
     return y
 
+
+def iterate_f(x, r, it):
+    y = logistic_map(x, r)
+    if it == 1:
+        return [y]
+    else:
+        return [y] + iterate_f(y, r, it-1)
+
+
+def iterate_f2(x, r, it):
+
+    l = []
+    y = x
+    for i in range(it):
+        y = logistic_map(y, r)
+        l.append(y)
+    return l
+
--- test_logistic_map.py
+++ test_logistic_map.py
@@ -1,10 +1,19 @@
 import numpy as np
 import pytest
 
-from logistic_map import logistic_map
+from logistic_map import logistic_map, iterate_f
 
 
 @pytest.mark.parametrize("x, r, expected", [(0.1, 2.2, 0.198), (0.2, 3.4, 0.544), (0.75, 1.7, 0.31875)])
 def test_logistic_map(x, r, expected):
     output = logistic_map(x, r)
     assert np.isclose(output, expected)
+
+
+
+@pytest.mark.parametrize("x, r, it, expected", [(0.1, 2.2, 1, [0.198]), (0.2, 3.4, 4, [0.544, 0.843418, 0.449019, 0.841163]), (0.75, 1.7, 2, [0.31875, 0.369152])])
+def test_iterate_f(x, r, it, expected):
+    output = iterate_f(x, r, it)
+    assert np.all(np.isclose(np.array(output), np.array(expected)))
+
+    
