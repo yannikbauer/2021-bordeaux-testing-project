c8175c4b6922182cc3e04d58d35a44abf7d26262
ASPP Student <student@localhost>
2021-08-24 15:00:00 +0200

    adapt order of iterate_f pos args

--- test_logistic_map.py
+++ test_logistic_map.py
@@ -13,7 +13,7 @@ def test_logistic_map(x, r, expected):
 
 @pytest.mark.parametrize("x, r, it, expected", [(0.1, 2.2, 1, [0.198]), (0.2, 3.4, 4, [0.544, 0.843418, 0.449019, 0.841163]), (0.75, 1.7, 2, [0.31875, 0.369152])])
 def test_iterate_f(x, r, it, expected):
-    output = iterate_f(x, r, it)
+    output = iterate_f(it, x, r)
     assert np.all(np.isclose(np.array(output), np.array(expected)))
 
     
